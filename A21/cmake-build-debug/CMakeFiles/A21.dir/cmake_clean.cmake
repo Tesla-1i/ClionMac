9733b0432a98c32ece00cc42461f5a8d8f548e06
sdmengxiangyu <OurEDA@Macintosh-77.local>
2016-12-28 11:21:34 +0800

    A21

---
+++ A21/cmake-build-debug/CMakeFiles/A21.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A21.dir/main.cpp.o"
+  "A21.pdb"
+  "A21"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A21.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
