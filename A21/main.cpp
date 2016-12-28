33933a379d1693ff5b1a3d78694e1e446f97a150
sdmengxiangyu <OurEDA@Macintosh-77.local>
2016-12-28 11:20:56 +0800

    A21

---
+++ A21/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void fun(int *w, int p, int n){
+    int i, j, t;
+    for(i=p; i<=n-1;i++){
+        t = w[n-1];
+        for(j=n-2;j>=0;j--)
+            w[j+1] = w[j];
+        w[0] = t;
+    }
+}
+
+int main(){
+    int w[7] = {1,2,3,4,5,6,7};
+    int p,i, n = 7;
+    printf("input p\n");
+    scanf("%d",&p);
+    fun(w, p, n);
+    for(i=0;i<7;i++){
+        printf("%d ",w[i]);
+    }
+    printf("\n");
+    return 0;
+}
