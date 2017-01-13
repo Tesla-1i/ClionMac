82a8973568cac2a30f353cb1b0cb5e4517a7e2fd
sdmengxiangyu <OurEDA@Macintosh-77.local>
2017-01-13 18:49:52 +0800

    cpp

--- A21/main.cpp
+++ A21/main.cpp
@@ -1,3 +1,5 @@
+#include <profile.h>
+
 /*
 #include <stdio.h>
 
@@ -23,6 +25,37 @@ int main(){
     printf("\n");
     return 0;
 }*/
+
+
+const int model = 90;
+const int v[] = {1,2,3,4};
+const int x = 0;
+void f(){
+    int model = 90;
+    model = 20;
+    const int v[] = {1,2,3,4};
+    //v[2]++;
+}
+
+void f1(char *p)
+{
+
+    char s[] = "Grom";
+    const char* pc = s;     //pc is a pointer to char const
+    //pc[3] = 'g';
+    pc = p;
+    char *const cp = s;     //cp is a const pointer to char
+    cp[3] = 'a';
+    const char *const cpc = s;      //cpc is a const pointer to const char
+    //cpc[3] = 'a';
+    //cpc = p;
+
+    const int c = 2;
+    const int *p1 = &c;
+    //int *p3 = &c;
+    
+}
+
 
 
 
